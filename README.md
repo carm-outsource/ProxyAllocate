91fb7fb83c8e4b67fef810b9412e2c4ae170926e
carm <carm@carm.cc>
2025-01-12 03:15:25 +0800

    feat: Project finished

---
+++ README.md
@@ -0,0 +1,57 @@
+# UltraMOTD 超级介绍展示
+
+[![CodeFactor](https://www.codefactor.io/repository/github/CarmJos/UltraMOTD/badge?s=b76fec1f64726b5f19989aace6adb5f85fdab840)](https://www.codefactor.io/repository/github/CarmJos/UltraMOTD)
+![CodeSize](https://img.shields.io/github/languages/code-size/CarmJos/UltraMOTD)
+[![Download](https://img.shields.io/github/downloads/CarmJos/UltraMOTD/total)](https://github.com/CarmJos/UltraMOTD/releases)
+[![Java CI with Maven](https://github.com/CarmJos/UltraMOTD/actions/workflows/maven.yml/badge.svg?branch=master)](https://github.com/CarmJos/UltraMOTD/actions/workflows/maven.yml)
+![Support](https://img.shields.io/badge/Minecraft-Java%201.8--Latest-blue)
+
+A super lightweight [Velocity](https://papermc.io/software/velocity) plugin that
+supports evenly teleporting players to different 
+default or forced-host servers when login.
+
+_No configurations and commands, just a simple function implementation._
+
+## Example
+
+In Velocity's `velocity.toml` configuration file, like
+
+```toml
+[servers]
+server1 = "127.0.0.1:1001"
+server2 = "127.0.0.1:1002"
+server3 = "127.0.0.1:1003"
+server4 = "127.0.0.1:1004"
+
+game1 = "127.0.0.1:2001"
+game2 = "127.0.0.1:2002"
+game3 = "127.0.0.1:2003"
+game4 = "127.0.0.1:2004"
+
+try = [
+    "server1", "server2", "server3", "server4"
+]
+
+[forced-hosts]
+"play.your-server.net" = [
+    "game1", "game2", "game3", "game4"
+]
+```
+
+after configured, when a player login to the proxy,
+
+if the player's IP is `play.your-server.net`,
+the player will be teleported to the server 
+with the least number of players in `game1`, `game2`, `game3`, `game4`.
+
+if there's no any forced-hosts matched,
+the player will be teleported to the tried servers
+with the least number of players in `server1`, `server2`, `server3`, `server4`.
+
+
+## Open source agreement
+
+The source code of this project uses  [GNU General Public License v3.0](https://opensource.org/licenses/GPL-3.0)
+License.
+
+
---
+++ src/main/java/cc/carm/outsource/plugin/proxyallocate/ProxyAllocate.java
@@ -0,0 +1,105 @@
+package cc.carm.outsource.plugin.proxyallocate;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.PostOrder;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.ServerPreConnectEvent;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.config.ProxyConfig;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.net.InetSocketAddress;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+
+@Plugin(id = "proxyallocate", name = "ProxyAllocate", version = "1.0.1",
+        description = "A simple plugin that supports evenly teleporting players to different default or forced-host servers when login.",
+        url = "https://github.com/carm-outsource/ProxyAllocate",
+        authors = {"CarmJos"}
+)
+public class ProxyAllocate {
+
+    protected static ProxyAllocate instance;
+
+    private final ProxyServer server;
+    private final Logger logger;
+
+    public static ProxyAllocate getInstance() {
+        return instance;
+    }
+
+    @Inject
+    public ProxyAllocate(ProxyServer server, Logger logger) {
+        this.server = server;
+        this.logger = logger;
+        ProxyAllocate.instance = this;
+    }
+
+    @Subscribe(order = PostOrder.FIRST)
+    public void onInitialize(ProxyInitializeEvent event) {
+        getServer().getEventManager().register(this, this);
+    }
+
+    public static ProxyServer getServer() {
+        return getInstance().server;
+    }
+
+    public @NotNull Logger getLogger() {
+        return logger;
+    }
+
+    public static void info(String message, Object... params) {
+        getInstance().getLogger().info(String.format(message, params));
+    }
+
+    public static void severe(String message, Object... params) {
+        getInstance().getLogger().severe(String.format(message, params));
+    }
+
+    @Subscribe
+    public void onServerConnected(ServerPreConnectEvent event) {
+        Player player = event.getPlayer();
+        if (player.getCurrentServer().isPresent()) return;
+
+        String domain = player.getVirtualHost().map(InetSocketAddress::getHostString).orElse("").toLowerCase();
+        ProxyConfig conf = ProxyAllocate.getServer().getConfiguration();
+        RegisteredServer target = pickServer(conf.getForcedHosts().get(domain));
+        if (target == null) {
+            target = pickServer(conf.getAttemptConnectionOrder());
+        }
+
+        if (target == null) {
+            // NO server available
+            event.setResult(ServerPreConnectEvent.ServerResult.denied());
+            return;
+        }
+        event.setResult(ServerPreConnectEvent.ServerResult.allowed(target));
+    }
+
+    public static @Nullable RegisteredServer pickServer(@Nullable Collection<String> servers) {
+        if (servers == null || servers.isEmpty()) return null;
+        if (servers.size() == 1) return ProxyAllocate.getServer().getServer(servers.iterator().next()).orElse(null);
+        Set<RegisteredServer> available = new HashSet<>();
+        servers.forEach(id -> ProxyAllocate.getServer().getServer(id).ifPresent(available::add));
+        return pickRegisteredServer(available);
+    }
+
+    public static @Nullable RegisteredServer pickRegisteredServer(@Nullable Collection<RegisteredServer> servers) {
+        if (servers == null || servers.isEmpty()) return null;
+        if (servers.size() == 1) return servers.iterator().next();
+        return servers.stream().min(
+                Comparator.comparingInt((RegisteredServer o) -> o.getPlayersConnected().size())
+                        .thenComparing(o -> o.getServerInfo().getName())
+        ).orElse(null);
+    }
+
+
+}
